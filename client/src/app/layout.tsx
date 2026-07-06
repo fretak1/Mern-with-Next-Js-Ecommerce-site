92a4f47052b13ead83f8fc3b7525fe3c8096820c
Frezer Takele <frezertakele1@gmail.com>
2026-07-06 11:40:58 +0300

    Refactor layout.tsx and update tsconfig.json for improved readability and configuration

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import CommonLayout from "@/components/common/layout";
-import { useAuthStore } from "@/store/useAuthStore";
-import { useEffect } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
